b0c51b6ef27ce4c97efe5ae1ba329d8bbd54f44e
sathsidu99 <sathsiduagresara@gmail.com>
2025-06-17 07:01:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,6 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "tdowxCob#9kbTr-ERDeQoqEj3IySBdc6w8757KXo6sHyiMT4AYD0",
   OWNER_NUM: process.env.OWNER_NUM || "94773259828",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/sathsidu99/PRINZY-MD-MEDIA/refs/heads/main/LOGO/alive.jpeg",
+  ALIVE_MSG: process.env.ALIVE_MSG|| "Hello , 𝐏𝐑𝐈𝐍𝐙𝐘 𝐌𝐃 🌈 is alive now!!\n\n> 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒𝐀𝐓𝐇𝐒𝐈𝐃𝐔 𝐀𝐆𝐑𝐄𝐒𝐀𝐑𝐀",
 };
