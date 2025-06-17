d406c649ee9987b86a526835fcb8428731c9447a
sathsidu99 <sathsiduagresara@gmail.com>
2025-06-17 10:10:25 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,10 +10,11 @@ cmd({
 },
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})    
-}
+return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek});
 await robin.sendPresenceUpdate('recording', from);
 await robin.sendMessage(from, { audio: { url: "https://github.com/sathsidu99/PRINZY-MD-MEDIA/raw/refs/heads/main/AUDIO/alive.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+}
+
 catch(e){
 console.log(e)
 reply(`${e}`)
