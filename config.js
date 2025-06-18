1cd1e2e85cedf60e0a6528213fa3af4f2d06c7a0
sathsidu99 <sathsiduagresara@gmail.com>
2025-06-18 17:24:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "tdowxCob#9kbTr-ERDeQoqEj3IySBdc6w8757KXo6sHyiMT4AYD0", // replace your session id 
-  OWNER_NUM: process.env.OWNER_NUM || "94773259828", // replace your number
+  SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID", // replace your session id 
+  OWNER_NUM: process.env.OWNER_NUM || "YOUR_NUMBER", // replace your number
   PREFIX: process.env.PREFIX || ".",  // enter your prefix ( ,/./@/$/%/& )
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/sathsidu99/PRINZY-MD-MEDIA/refs/heads/main/LOGO/alive.jpeg", // dont replace 
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , 𝐏𝐑𝐈𝐍𝐙𝐘 𝐌𝐃 🌈 is alive now!!\n\n> 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒𝐀𝐓𝐇𝐒𝐈𝐃𝐔 𝐀𝐆𝐑𝐄𝐒𝐀𝐑𝐀", // dont replace 
